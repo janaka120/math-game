9f907b31af227e5f637811e55b21f07c2e27434f
janaka120 <janakastill@gmail.com>
2023-06-23 08:34:14 +0800

    initial commit msg

---
+++ app/src/main/java/com/example/mathgame/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.example.mathgame
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    lateinit var addition: Button
+    lateinit var subtraction: Button
+    lateinit var multifiction: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        addition = findViewById(R.id.buttonAdd)
+        subtraction = findViewById(R.id.buttonSub)
+        multifiction = findViewById(R.id.buttonMulti)
+
+        addition.setOnClickListener {
+            val intent = Intent(this@MainActivity, GameActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
